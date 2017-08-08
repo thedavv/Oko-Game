97e6666a1033dd5edeec45e484c064798f248c4f
hubulubu <hubulubu@davv>
2017-08-09 00:48:01 +0200

    added todo coments
    added more tests

--- src/app/Start.java
+++ src/app/Start.java
@@ -11,7 +11,7 @@ import model.cardproperty.*;
 import util.DimensionsException;
 import util.Settings;
 
-public class Start {
+public class Start {//TODO implement settings to card factory
 	public static void main(String[] args) {
 		Settings s = Settings.getInstance();
 		
@@ -21,9 +21,17 @@ public class Start {
 			e.printStackTrace();
 		}
 		
+		System.out.println("First test");
 		test1();
+		System.out.println("Second test");
+		System.out.println();
 		test2();
+		System.out.println("third test");
+		System.out.println();
 		test3();
+		System.out.println("fourth test");
+		System.out.println();
+		test4();
 		
 	}
 	
@@ -106,5 +114,32 @@ public class Start {
 		ch.drawBothPlayersSecondStyle(player, computer);
 	}
 	
+	private static void test4(){
+		CardFactory cf = new CardFactory();
+		Card eightHearths = cf.createCard(Color.HEARTHS, Value.ACE);
+		Card sevenHearths = cf.createCard(Color.SPADES, Value.JACK);
+		Card nineClubs = cf.createCard(Color.CLUBS, Value.NINE);
+		Card tenDiamonds = cf.createCard(Color.DIAMONDS, Value.TEN);
+		
+		List<Card> cards = new ArrayList<>();
+		List<Card> cards2 = new ArrayList<>();
+		
+		cards.add(sevenHearths);
+		cards.add(eightHearths);
+		cards.add(nineClubs);
+		cards2.add(tenDiamonds);
+		cards2.add(nineClubs);
+		
+		Player player = new Player();
+		Player computer = new Player();
+		
+		player.setCards(cards);
+		computer.setCards(cards2);
+		
+		CardFactoryHandler ch = new CardFactoryHandler();
+		//ch.drawPlayer(player);
+		ch.drawGameBoard(player, computer, 1);
+		ch.drawGameBoard(player, computer, 2);
+	}
 }
 
