01825087e50fa751cc7ac2b63cc44047b64e3dcd
hubulubu <hubulubu@davv>
2017-08-09 00:14:09 +0200

    changed name of factory method

--- src/model/cardfactory/CardFactory.java
+++ src/model/cardfactory/CardFactory.java
@@ -4,7 +4,7 @@ import model.cardproperty.Color;
 import model.cardproperty.Value;
 
 public class CardFactory {
-	public Card getCard(Color color, Value value){
+	public Card createCard(Color color, Value value){
 		return new PlayerCard(color, value);	
 	};
 }
