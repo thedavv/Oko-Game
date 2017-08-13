26083a7afb4ee717830b3b4604cd05be08c5c764
thedavv <thedavv@users.noreply.github.com>
2017-08-13 23:55:16 +0200

    Added int value of labels

--- src/model/cardproperty/Value.java
+++ src/model/cardproperty/Value.java
@@ -9,16 +9,22 @@ import util.Const;
  *
  */
 public enum Value {
-	SEVEN(Const.SEVEN), EIGHT(Const.EIGHT), NINE(Const.NINE), TEN(Const.TEN), JACK(Const.JACK), QUEEN(Const.QUEEN), KING(Const.KING), ACE(Const.ACE);
+	SEVEN(Const.SEVEN, 7), EIGHT(Const.EIGHT, 8), NINE(Const.NINE, 9), TEN(Const.TEN, 10), JACK(Const.JACK, 1), QUEEN(Const.QUEEN, 1), KING(Const.KING, 1), ACE(Const.ACE, 1);
 
+	private final int intValue;
 	private final String label;
 
-	Value(String label){
+	Value(String label, int intValue){
 		this.label = label;
+		this.intValue = intValue;
 	}
 
 	public String getLabel(){
 		return this.label;
+	}
+	
+	public int getIntValue(){
+		return this.intValue;
 	}
 
 }
